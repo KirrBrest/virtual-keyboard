c278ad1adca5ed49450a855f1c83ae12d5d9c6f2
Kiryl Lukashchuk <lukashchuk.kiryl@yandex.by>
2023-05-01 13:19:36 +0300

    feat: changes button's styles (Mon, May 01, 2023 01:19:35 PM)

--- virtual-keyboard/js/script.js
+++ virtual-keyboard/js/script.js
@@ -22,35 +22,34 @@ function init () {
   let out = '';
   for (let i = 0; i < keyboard.length; i++) {
     if (i == 13) {
-      out += '<div class = "big-key k-key"> Backspace </div>';
+      out += '<div class = "big-button k-key"> Backspace </div>';
       out += '<div class = "tab k-key"> Tab </div>';
     } else if (i == 26) {
-      out += '<div class = "keys k-key"> Del </div>';
-      out += '<div class = "big-key k-key"> CapsLock </div>';
+      out += '<div class = "service-button k-key"> Del </div>';
+      out += '<div class = "big-button k-key"> CapsLock </div>';
     } else if (i == 37) {
-      out += '<div class = "double-key k-key"> Enter </div>';
-      out += '<div class = "big-key k-key"> Shift </div>';
+      out += '<div class = "double-button k-key"> Enter </div>';
+      out += '<div class = "big-button k-key"> Shift </div>';
     }
-    out += '<div class = "k-key keys" data = "' + keyboard[i] + '">' + keyboard[i] + '</div>';
+    out += '<div class = "button k-key" data = "' + keyboard[i] + '">' + keyboard[i] + '</div>';
      if (i == 46) {
-      out += '<div class = "arrows-key k-key"> UP </div>';
-      out += '<div class = "double-key k-key"> Shift </div>';
-      out += '<div class = "keys k-key"> Ctrl </div>';
-      out += '<div class = "keys k-key"> Win </div>';
-      out += '<div class = "keys k-key"> Alt </div>';
+      out += '<div class = "arrows-key k-key"><div class = "arrow-up"> </div></div>';
+      out += '<div class = "double-button k-key"> Shift </div>';
+      out += '<div class = "service-button k-key"> Ctrl </div>';
+      out += '<div class = "service-button k-key"> Win </div>';
+      out += '<div class = "service-button k-key"> Alt </div>';
       out += '<div class = "space k-key">  </div>';
-      out += '<div class = "keys k-key"> Alt </div>';
-      out += '<div class = "arrows-key k-key"> Left </div>';
-      out += '<div class = "arrows-key k-key"> Down </div>';
-      out += '<div class = "arrows-key k-key"> Right </div>';
-      out += '<div class = "keys k-key k-key"> Ctrl </div>';
+      out += '<div class = "service-button k-key"> Alt </div>';
+      out += '<div class = "arrows-key k-key"><div class = "arrow-left"> </div></div>';
+      out += '<div class = "arrows-key k-key"><div class = "arrow-down"> </div></div>';
+      out += '<div class = "arrows-key k-key"><div class = "arrow-right"> </div></div>';
+      out += '<div class = "service-button k-key k-key"> Ctrl </div>';
     }
   }
   document.querySelector('.keyboard-wrapper').innerHTML = out;
 }
 init ();
 
-// document.onkeypress = function (event) {
 document.addEventListener('keydown', function (event) {
   console.log(event.code);
   console.log(event.key);
@@ -67,27 +66,17 @@ document.addEventListener('keyup', function (event) {
 });
 
 document.querySelectorAll('.keyboard-wrapper .k-key').forEach(function (element) {
-  element.onclick = function (event) {
-    document.querySelectorAll('.keyboard-wrapper .k-key').forEach(function (element) {
-      element.classList.remove('active');
-    });
+  element.addEventListener("mousedown", function (event) {
     let key = this.getAttribute('data');
     this.classList.add('active');
+    textArea.innerText = key;
+
     console.log(key);
-  }
+  });
 });
 
-// document.addEventListener("mousedown", function (event) {
-//   console.log(event.code);
-//   console.log(event.key);
-//   document.querySelectorAll('.keyboard-wrapper .k-key').forEach(function (element) {
-//     element.classList.remove('active');
-//   });
-//   document.querySelector('.keyboard-wrapper .k-key[data = "' + event.key + '"]').classList.add('active');
-// });
-
-// document.addEventListener("mouseup", function (event) {
-//   document.querySelectorAll('.keyboard-wrapper .k-key').forEach(function (element) {
-//     element.classList.remove('active');
-//   });
-// });
+document.querySelectorAll('.keyboard-wrapper .k-key').forEach(function (element) {
+  element.addEventListener("mouseup", function (event) {
+    this.classList.remove('active');
+   });
+});
