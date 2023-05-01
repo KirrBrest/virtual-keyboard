1d12ba7a0aa04104f02d84d6c4a4cf3943e4026a
Kiryl Lukashchuk <lukashchuk.kiryl@yandex.by>
2023-05-01 20:02:23 +0300

    fix: chenges features mouse-clicks and input with buttons (Mon, May 01, 2023 08:02:22 PM)

--- virtual-keyboard/js/script.js
+++ virtual-keyboard/js/script.js
@@ -2,6 +2,7 @@ let textArea = document.createElement('textarea');
 textArea.className = "text-area";
 textArea.innerHTML = " ";
 document.body.append(textArea);
+//let mainText = text-area.value;
 
 
 var virtualKeyboard = document.createElement('div');
@@ -10,8 +11,11 @@ virtualKeyboard.innerHTML = "<strong>Virtual Keyboard</strong>";
 document.body.append(virtualKeyboard);
 
 let keyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'];
+let keyboardUp = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'k', 'L', ':', '"', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'];
+let keyboardRus = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.'];
+let keyboardRusUp = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ','];
 
-// отслеживаем символы  TODO - удалить перед деплоем!!!!!
+//отслеживаем символы  TODO - удалить перед деплоем!!!!!
 //  document.onkeypress = function (event) {
 //    console.log(event);
 //   keyboard.push(event.key);
@@ -65,14 +69,25 @@ document.addEventListener('keyup', function (event) {
   });
 });
 
+
 document.querySelectorAll('.keyboard-wrapper .k-key').forEach(function (element) {
   element.addEventListener("mousedown", function (event) {
     let key = this.getAttribute('data');
     this.classList.add('active');
-    textArea.innerText = key;
-
+    if (key !== null) {
+      textArea.value += key;
+    }
     console.log(key);
   });
+
+});
+
+document.addEventListener("keydown", function (event) {
+  let push = event.key;
+  if (push !== null && push.length < 2) {
+    textArea.value += push;
+  }
+  console.log("push");
 });
 
 document.querySelectorAll('.keyboard-wrapper .k-key').forEach(function (element) {
